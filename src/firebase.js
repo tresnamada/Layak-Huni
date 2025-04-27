86f133a54d894cc85da9efd8e5299c25f8da2637
Erlangga Tresnamada Muliawan <tresnamada@gmail.com>
2025-04-27 20:08:36 +0700

    (+) Marketplace Page

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyALZ5_coEBJKe2Qbs0MeW7XHCWluGj42zw",
+  authDomain: "siaphuni-8ee48.firebaseapp.com",
+  projectId: "siaphuni-8ee48",
+  storageBucket: "siaphuni-8ee48.firebasestorage.app",
+  messagingSenderId: "121222743559",
+  appId: "1:121222743559:web:d950f5ce3f053b62ad4ca2",
+  measurementId: "G-YDXQG62EFC"
+};
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Taruh Firebasenya Key Disini
